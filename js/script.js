3a89721f0b9ad4773363bebb9a0f0e55c9c6a79f
phamcaohung <phamcao77@gmail.com>
2024-12-15 02:02:54 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing", {
+    strings: ["Developer", "Software Engineer", "Front-End Developer", "Back-End Developer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
